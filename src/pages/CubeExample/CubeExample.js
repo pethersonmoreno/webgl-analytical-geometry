cdb5f621eb50bdd132c19435298d378590a69f5c
Petherson Moreno <petherson.moreno@ingaia.com.br>
2019-05-30 13:10:59 -0300

    feat(CubeExample): added rotation using mouse and touch

--- src/pages/CubeExample/CubeExample.js
+++ src/pages/CubeExample/CubeExample.js
@@ -1,19 +1,99 @@
-import React, { Component } from "react";
-import "./CubeExample.css";
-import CanvasCube from "./CanvasCube";
+import React, { Component } from 'react';
+import './CubeExample.css';
+import CanvasCube from './CanvasCube';
 
+const getDiffPixelsInRadians = pixels => pixels / 800.0;
+
+const getPosFromTouch = touch => ({
+  x: touch.clientX,
+  y: touch.clientY,
+});
+const getPosFromMouseEvent = event => ({
+  x: event.clientX,
+  y: event.clientY,
+});
 class CubeExample extends Component {
   state = {
     rotationX: 0.0,
     rotationY: 0.7,
-    rotationZ: 1.0
+    rotationZ: 1.0,
+    pos: null,
   };
+  onTouchStart = e => {
+    this.setState({
+      pos: getPosFromTouch(e.touches[0]),
+    });
+  };
+
+  onTouchMove = e => {
+    if (!e.changedTouches || !e.changedTouches.length) {
+      return;
+    }
+    this.onUpdatePos(getPosFromTouch(e.changedTouches[0]));
+  };
+
+  onTouchEnd = e => {
+    this.setState({
+      pos: null,
+    });
+  };
+
+  onMouseDown = e => {
+    this.setState({
+      pos: getPosFromMouseEvent(e),
+    });
+  };
+
+  onMouseMove = e => {
+    this.onUpdatePos(getPosFromMouseEvent(e));
+  };
+
+  onMouseUp = e => {
+    console.log(e);
+    this.setState({
+      pos: null,
+    });
+  };
+
+  onMouseOut = e => {
+    this.setState({
+      pos: null,
+    });
+  };
+
+  onUpdatePos = newPos =>
+    this.setState(state => {
+      const { pos, rotationX, rotationY } = this.state;
+      if (!pos) {
+        return state;
+      }
+      const newRotationX =
+        rotationX + -getDiffPixelsInRadians(pos.x - newPos.x);
+      const newRotationY =
+        rotationY + -getDiffPixelsInRadians(pos.y - newPos.y);
+      return {
+        rotationX: newRotationX,
+        rotationY: newRotationY,
+        pos: newPos,
+      };
+    });
+
   render() {
     const { rotationX, rotationY, rotationZ } = this.state;
     return (
       <div className="page">
         <h1>Cube Example</h1>
-        <div className="containerCanvas">
+        <div
+          className="containerCanvas"
+          onTouchStart={this.onTouchStart}
+          onTouchMove={this.onTouchMove}
+          onTouchEnd={this.onTouchEnd}
+          onMouseDown={this.onMouseDown}
+          onMouseMove={this.onMouseMove}
+          onMouseUp={this.onMouseUp}
+          onMouseOut={this.onMouseOut}
+        >
+          >
           <CanvasCube
             rotationX={rotationX}
             rotationY={rotationY}
@@ -47,31 +127,27 @@ class CubeExample extends Component {
             onChange={event => this.setState({ rotationZ: event.target.value })}
           />
         </p>
-        <p>
-          Source Code:
-          <ul>
-            <li>
-              <a href="https://github.com/pethersonmoreno/webgl-analytical-geometry/tree/feature/first-webgl-with-react/src/pages/CubeExample">
-                CubeExample
-              </a>
-            </li>
-          </ul>
-        </p>
-        <p>
-          Done based on:
-          <ul>
-            <li>
-              <a href="https://mdn.github.io/webgl-examples/tutorial/sample5/index.html">
-                Sample 5 of webgl-examples
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/mdn/webgl-examples/tree/gh-pages/tutorial/sample5">
-                Source Code of Sample 5 of webgl-examples
-              </a>
-            </li>
-          </ul>
-        </p>
+        <p>Source Code:</p>
+        <ul>
+          <li>
+            <a href="https://github.com/pethersonmoreno/webgl-analytical-geometry/tree/feature/first-webgl-with-react/src/pages/CubeExample">
+              CubeExample
+            </a>
+          </li>
+        </ul>
+        <p>Done based on:</p>
+        <ul>
+          <li>
+            <a href="https://mdn.github.io/webgl-examples/tutorial/sample5/index.html">
+              Sample 5 of webgl-examples
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/mdn/webgl-examples/tree/gh-pages/tutorial/sample5">
+              Source Code of Sample 5 of webgl-examples
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
